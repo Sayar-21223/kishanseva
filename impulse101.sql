1d3f59b2a15ea1670ee086ded260e9674eef214d
Sugnik Tarafder <115608482+ArjoSugnik@users.noreply.github.com>
2024-03-02 12:39:54 +0530

    database

---
+++ impulse101.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 28, 2024 at 09:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `impulse101`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyerregistration`
+--
+
+CREATE TABLE `buyerregistration` (
+  `buyer_id` int(255) NOT NULL,
+  `buyer_name` varchar(30) NOT NULL,
+  `buyer_phone` bigint(10) NOT NULL,
+  `buyer_addr` text NOT NULL,
+  `buyer_comp` varchar(100) NOT NULL,
+  `buyer_license` varchar(100) NOT NULL,
+  `buyer_bank` int(16) NOT NULL,
+  `buyer_pan` varchar(10) NOT NULL,
+  `buyer_mail` varchar(20) NOT NULL,
+  `buyer_username` varchar(20) NOT NULL,
+  `buyer_password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `product_id` int(255) NOT NULL,
+  `phonenumber` bigint(10) NOT NULL,
+  `qty` int(10) NOT NULL DEFAULT 1,
+  `subtotal` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`product_id`, `phonenumber`, `qty`, `subtotal`) VALUES
+(32, 8169193101, 2, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_title` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
+(1, 'Crops'),
+(2, 'Vegetables'),
+(3, 'Fruits');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consumer`
+--
+
+CREATE TABLE `consumer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `pincode` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `pincode` varchar(6) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `clinicName` varchar(15) NOT NULL,
+  `clinicAddress` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `startTime` varchar(8) NOT NULL,
+  `endTime` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farmerregistration`
+--
+
+CREATE TABLE `farmerregistration` (
+  `farmer_id` int(255) NOT NULL,
+  `farmer_name` varchar(255) NOT NULL,
+  `farmer_phone` bigint(10) NOT NULL,
+  `farmer_address` text NOT NULL,
+  `farmer_state` varchar(50) NOT NULL,
+  `farmer_district` varchar(50) NOT NULL,
+  `farmer_pan` varchar(10) NOT NULL,
+  `farmer_bank` int(16) NOT NULL,
+  `farmer_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(255) NOT NULL,
+  `product_id` int(255) NOT NULL,
+  `qty` int(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `delivery` varchar(10) NOT NULL,
+  `phonenumber` bigint(10) NOT NULL,
+  `total` int(10) NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `buyer_phonenumber` bigint(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan_applications`
+--
+
+CREATE TABLE `loan_applications` (
+  `application_id` int(11) NOT NULL AUTO_INCREMENT,
+  `applicant_name` varchar(255) NOT NULL,
+  `applicant_phone` bigint(10) NOT NULL,
+  `applicant_email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `pan_no` varchar(10) NOT NULL,
+  `account_no` varchar(16) NOT NULL,
+  `loan_amount` decimal(10,2) NOT NULL,
+  `year` int(4) NOT NULL,
+  `application_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`application_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(100) NOT NULL,
+  `farmer_fk` int(255) NOT NULL,
+  `product_title` varchar(100) NOT NULL,
+  `product_cat` varchar(100) NOT NULL,
+  `product_type` varchar(100) NOT NULL,
+  `product_expiry` varchar(25) NOT NULL,
+  `product_image` text NOT NULL,
+  `product_stock` int(100) NOT NULL,
+  `product_price` int(100) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_keywords` text NOT NULL,
+  `product_delivery` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopkeeper`
+--
+
+CREATE TABLE `shopkeeper` (
+  `id` int(255) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `pincode` varchar(6) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `shopName` varchar(20) NOT NULL,
+  `shopAddress` varchar(50) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `startTime` varchar(8) NOT NULL,
+  `endTime` varchar(8) NOT NULL,
+  `slotInterval` int(11) NOT NULL,
+  `slotUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slot`
+--
+
+CREATE TABLE `slot` (
+  `slot_id` int(255) NOT NULL,
+  `shop_id` int(255) NOT NULL,
+  `slot` varchar(10) NOT NULL,
+  `vacancy` int(255) NOT NULL,
+  `date` varchar(12) NOT NULL,
+  `phonenumber` bigint(10) NOT NULL,
+  `passcode` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buyerregistration`
+--
+ALTER TABLE `buyerregistration`
+  ADD PRIMARY KEY (`buyer_id`),
+  ADD UNIQUE KEY `buyer_username` (`buyer_username`),
+  ADD UNIQUE KEY `buyer_phone` (`buyer_phone`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `consumer`
+--
+ALTER TABLE `consumer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `farmerregistration`
+--
+ALTER TABLE `farmerregistration`
+  ADD UNIQUE KEY `farmer_id` (`farmer_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `phonenumber` (`phonenumber`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `farmer_fk` (`farmer_fk`);
+
+--
+-- Indexes for table `shopkeeper`
+--
+ALTER TABLE `shopkeeper`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slot`
+--
+ALTER TABLE `slot`
+  ADD PRIMARY KEY (`slot_id`),
+  ADD UNIQUE KEY `passcode` (`passcode`),
+  ADD KEY `slot_ibfk_1` (`shop_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buyerregistration`
+--
+ALTER TABLE `buyerregistration`
+  MODIFY `buyer_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `consumer`
+--
+ALTER TABLE `consumer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `farmerregistration`
+--
+ALTER TABLE `farmerregistration`
+  MODIFY `farmer_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `shopkeeper`
+--
+ALTER TABLE `shopkeeper`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `slot`
+--
+ALTER TABLE `slot`
+  MODIFY `slot_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `slot`
+--
+ALTER TABLE `slot`
+  ADD CONSTRAINT `slot_ibfk_1` FOREIGN KEY (`shop_id`) REFERENCES `shopkeeper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
